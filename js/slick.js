60cf389c93765057e907722a738049c8d7782385
Ihly <aslak.ihle@gmail.com>
2017-03-05 20:51:25 +0100

    hue

--- js/slick.js
+++ js/slick.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
-    $('.carousel').slick({
-        setting-name: setting - value
+$(document).on('ready', function() {
+    $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
     });
+    /*
+    $(".center").slick({
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+    $(".variable").slick({
+        dots: true,
+        infinite: true,
+        variableWidth: true
+    });
+    $(".lazy").slick({
+        lazyLoad: 'ondemand', // ondemand progressive anticipated
+        infinite: true
+    });
+    */
 });
+</script>
