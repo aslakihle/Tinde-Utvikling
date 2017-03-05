3036baa4452249d10e46ec7f10fb5c21974ca342
Ihly <aslak.ihle@gmail.com>
2017-03-05 21:14:35 +0100

    hue

--- js/slick.js
+++ js/slick.js
@@ -1,27 +1,32 @@
 $(document).on('ready', function() {
-    $(".regular").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-    /*
-    $(".center").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-    });
-    $(".variable").slick({
-        dots: true,
-        infinite: true,
-        variableWidth: true
-    });
-    $(".lazy").slick({
-        lazyLoad: 'ondemand', // ondemand progressive anticipated
-        infinite: true
-    });
-    */
+	$(".regular").slick({
+		dots: true,
+		infinite: true,
+		autoplay: true,
+		arrows: true,
+		appendDots: $(".navDots"),
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		//prevArrow: $(".prev"),
+		nextArrow: $(".next")
+	});
+
+	/*
+	$(".center").slick({
+		dots: true,
+		infinite: true,
+		centerMode: true,
+		slidesToShow: 3,
+		slidesToScroll: 3
+	});
+	$(".variable").slick({
+		dots: true,
+		infinite: true,
+		variableWidth: true
+	});
+	$(".lazy").slick({
+		lazyLoad: 'ondemand', // ondemand progressive anticipated
+		infinite: true
+	});
+	*/
 });
-</script>
