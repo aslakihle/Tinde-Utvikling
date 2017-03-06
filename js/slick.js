7639b952fc12971808cb6a61deaa656b1f26ced2
Ihly <aslak.ihle@gmail.com>
2017-03-06 15:29:27 +0100

    Revert "hue"
    This reverts commit 3ec47e5ca820dee88a0f8416864f1aed3e01698b.

--- js/slick.js
+++ js/slick.js
@@ -3,11 +3,30 @@ $(document).on('ready', function() {
 		dots: true,
 		infinite: true,
 		autoplay: true,
-		autoplaySpeed: 10000,
+		arrows: true,
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		arrows: true,
+		appendDots: $(".navDots"),
 		prevArrow: $(".prev"),
 		nextArrow: $(".next")
 	});
+
+	/*
+	$(".center").slick({
+		dots: true,
+		infinite: true,
+		centerMode: true,
+		slidesToShow: 3,
+		slidesToScroll: 3
+	});
+	$(".variable").slick({
+		dots: true,
+		infinite: true,
+		variableWidth: true
+	});
+	$(".lazy").slick({
+		lazyLoad: 'ondemand', // ondemand progressive anticipated
+		infinite: true
+	});
+	*/
 });
